b2da773eeb7af44195ced85dbb3b455a79375e2d
Paul Gellai <inbox@paulgg.com>
2017-10-31 22:33:00 -0700

    first trap

---
+++ Assets/Script/Hazards/FallingHazard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingHazard : MonoBehaviour {
+
+
+    public GameObject projectile;
+    public GameObject plane;
+    public float trapDuration;
+    float startTime = 0;
+    bool started = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(projectile == null)
+        {
+            if(!started)
+            {
+                startTime = Time.time;
+                started = true;
+            }
+            Debug.Log("Start time: " + startTime);
+            Debug.Log("Current time: " + Time.time);
+            if (Time.time - trapDuration >= startTime)
+            {
+                DestroyObject(gameObject);
+            }
+        }
+	}
+}
---
+++ Assets/Script/Hazards/FallingHazardHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingHazardHelper : MonoBehaviour {
+
+    GameObject projectile;
+    GameObject plane;
+
+    // Use this for initialization
+    void Start () {
+        projectile = gameObject.transform.parent.GetChild(0).gameObject;
+        plane = gameObject.transform.parent.GetChild(1).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        plane.transform.SetPositionAndRotation(gameObject.transform.position, Quaternion.identity);
+        plane.SetActive(true);
+        for (int i = 0; i < plane.transform.childCount; i++)
+        {
+            plane.transform.GetChild(i).gameObject.SetActive(true);
+        }
+        DestroyObject(gameObject);
+    }
+}
