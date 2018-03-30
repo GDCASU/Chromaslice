7657bf8a9bb241c10a139c8c19ead28d78b8c40f
Keterr <kartelant@gmail.com>
2018-03-30 04:01:39 -0700

    implement current control config menu

--- Assets/Script/TitleMenu.cs
+++ Assets/Script/TitleMenu.cs
@@ -12,11 +12,19 @@ using UnityEngine.UI;
 public class TitleMenu : MonoBehaviour
 {
 
+    //local game
     public Toggle[] roundSelect;
     public GameObject[] deathmatchLevelSelect;
     public GameObject[] soccerLevelSelect;
     public GameObject DeathmatchPanel;
     public GameObject SoccerPanel;
+
+    //controls
+    public Dropdown selectedPlayer;
+    public Text playerText;
+    public Button dashButton;
+    public Button powerUpButton;
+    public Dropdown selectedMovement;
 
     private string selectedLevel;
     private int rounds;
@@ -99,6 +107,83 @@ public class TitleMenu : MonoBehaviour
                 break;
         }
 
+    }
+
+    public void PopulateControls()
+    {
+        int player = selectedPlayer.value + 1;
+        playerText.text = "Player " + player;
+        Controls c = Controls.LoadFromConfig(player);
+        dashButton.GetComponentInChildren<Text>().text = c.dashButton.keyPos.ToString();
+        powerUpButton.GetComponentInChildren<Text>().text = c.powerUpButton.keyPos.ToString();
+        if (c.horizAxis.axis.EndsWith(".5"))
+            selectedMovement.value = 1;
+        else if (c.horizAxis.axis.EndsWith(".8"))
+            selectedMovement.value = 2;
+        else
+            selectedMovement.value = 0;
+    }
+
+    public void ChangeDashButton()
+    {
+        StartCoroutine(ChangeControl(selectedPlayer.value, true));        
+    }
+
+    public void ChangePowerupButton()
+    {
+        StartCoroutine(ChangeControl(selectedPlayer.value, false));
+    }
+
+    public void ChangeMovement()
+    {
+        int player = selectedPlayer.value + 1;
+        Controls c = Controls.LoadFromConfig(player);
+        switch(selectedMovement.value)
+        {
+            case 0:
+                c.horizAxis = new Controls.ControlInput("Horizontal" + player);
+                c.vertAxis = new Controls.ControlInput("Vertical" + player);
+                break;
+            case 1:
+                c.horizAxis = new Controls.ControlInput("Horizontal" + player + ".5");
+                c.vertAxis = new Controls.ControlInput("Vertical" + player + ".5");
+                break;
+            case 2:
+                c.horizAxis = new Controls.ControlInput("Horizontal" + player + ".8");
+                c.vertAxis = new Controls.ControlInput("Vertical" + player + ".8");
+                break;
+        }
+
+        Controls.SaveToConfig(c);
+    }
+
+    public IEnumerator ChangeControl(int player, bool dash)
+    {
+        if (dash)
+        {
+            dashButton.interactable = false;
+            dashButton.GetComponentInChildren<Text>().text = "Press a button";
+        }
+        else
+        {
+            powerUpButton.interactable = false;
+            powerUpButton.GetComponentInChildren<Text>().text = "Press a button";
+        }
+        Debug.Log("Waiting for input...");
+        Controls c = Controls.LoadFromConfig(player+1);
+        Controls.ControlInput input;
+        if (dash)
+            input = c.dashButton;
+        else
+            input = c.powerUpButton;
+        yield return StartCoroutine(Controls.WaitForInput(input));
+        Debug.Log("Found input. Saving...");
+        Controls.SaveToConfig(c);
+        if (dash)
+            dashButton.interactable = true;
+        else
+            powerUpButton.interactable = true;
+        PopulateControls();
     }
 
     public void ExitGame()
