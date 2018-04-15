039cb98d89b61249daf99a7a7bab06852ab5b5cc
excelo <excel.ortega@gmail.com>
2018-04-14 22:22:32 -0700

    Made Lobby UI world space and created ui controller to make it easier to interact with ui using controller

---
+++ Assets/Script/UIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIController : MonoBehaviour 
+{
+    public GameObject defaultButtonSelected;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        // this won't work with networking, idk lol
+        if (Input.GetAxis("Vertical1") != 0 && EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(defaultButtonSelected);   
+        }
+	}
+
+    public void SetDefaultButton(GameObject button)
+    {
+        defaultButtonSelected = button;
+        EventSystem.current.SetSelectedGameObject(defaultButtonSelected);   
+    }
+}
