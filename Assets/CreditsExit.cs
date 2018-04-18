f13546a41e317e99ea5228b149a27629824285ce
Kyle Chapman <xeuorux@gmail.com>
2018-04-18 16:00:00 -0700

    Skipabble and fastwordable credits, disabled powerup spawners in each level

---
+++ Assets/CreditsExit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsExit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyUp(KeyCode.Escape))
+		{
+			SoundManager.singleton.ResetLevelMusic("ChromasliceTheme");
+            SceneManager.LoadScene("Title");
+        }
+
+		if (Input.GetKeyUp(KeyCode.Space) && Time.timeScale != 2.5f)
+		{
+			Time.timeScale = 5f;
+        }
+	}
+
+	void OnDestroy()
+	{
+		Time.timeScale = 1.0f;
+	}
+
+}
